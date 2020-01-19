270e159cbf7b9f87b245f361612ba7e3d78d43a4
lucasgdo <60046314+lucasgdo@users.noreply.github.com>
2020-01-19 18:45:17 -0300

    alterando para python 3

--- cli.py
+++ cli.py
@@ -6,10 +6,10 @@ import sys
 if __name__ == "__main__":
     _estacoes = core._busca(sys.argv[1]) if len(sys.argv) > 1 else core._get()
     i=0
-    print "PROJETO BICICLETAR - FORTALEZA/BR"
+    print ("PROJETO BICICLETAR - FORTALEZA/BR")
     for _estacao in _estacoes:
         _nome, _lat, _long, _endereco, _linha, _statusOnline, _StatusOperacional, _disp1, _disp2, _total, _internalStatus, _img, _id = _estacao
-        print 'estacao id %d:' % _id, _nome, '-', _linha, '-', _endereco, ' / bikes disponiveis: %d, vagas livres: %d' % (int(_disp2),int(_total))
+        print ('estacao id %d:' % _id, _nome, '-', _linha, '-', _endereco, ' / bikes disponiveis: %d, vagas livres: %d' % (int(_disp2),int(_total)))
         i+=1
-    print 'foram mostradas %d estacoes' % i
+    print ('foram mostradas %d estacoes' % i)
  
